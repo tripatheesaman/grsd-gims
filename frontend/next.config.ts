3fd51705f94a16aa6696f30d1041350f09dc37ff
Saman Tripathee <tripatheesaman1@gmail.com>
2026-02-05 12:04:22 +0545

    Standalone output for next

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from "next";
 const BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH || "";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   basePath: BASE_PATH,
   assetPrefix: BASE_PATH ? `${BASE_PATH}/` : undefined,
 
